b9cf316aaf45f09a589001b2f78cf9eb81bcd3a2
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-17 12:13:47 +0100

    Add db helpers

---
+++ data/helpers/cars.js
@@ -0,0 +1,18 @@
+const knex = require('knex');
+const db = knex(require('./knexfile').development);
+
+
+function find() {
+    return db('cars');
+}
+
+function findByID(id) {
+    return db('cars')
+        .where({ id: Number(id) });
+}
+
+function insert(data) {
+    return db('cars')
+        .insert(data)
+        .then(ids => ({ id: ids[0] }));
+}
